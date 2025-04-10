ac09a20f16a9e739f0642e7c46f49c2f89a86a2a
Bárkányi Dávid <h255258@stud.u-szeged.hu>
2025-04-10 18:29:19 +0200

    Bejelentkezés GUI, Input beolvasás, Regisztráció Activityre átvezetés

--- app/src/main/java/com/example/okmanyirodaugyintezes/MainActivity.java
+++ app/src/main/java/com/example/okmanyirodaugyintezes/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.okmanyirodaugyintezes;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,87 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
+    // CONSTS
+    private static final String LOG_TAG = MainActivity.class.getName();
+    private static final String PREF_KEY = Objects.requireNonNull(MainActivity.class.getPackage()).toString();
+    private static final int SECRET_KEY = 99;
+
+
+    // FIELDS
+    EditText usernameEditText;
+    EditText passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        this.usernameEditText = findViewById(R.id.usernameEditText);
+        this.passwordEditText = findViewById(R.id.passwordEditText);
+
+        Log.i(LOG_TAG, "onCreate");
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    // ACTIONS
+    public void login(View view) {
+        String userName = usernameEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+
+        Log.i(LOG_TAG, "Bejelentkezett: " + userName + ", jelszó: " + password);
+    }
+
+    public void register(View view) {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        intent.putExtra("SECRET_KEY", SECRET_KEY);
+        startActivity(intent);
+    }
+
+
+    // OVERRIDES
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(LOG_TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(LOG_TAG, "onResume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(LOG_TAG, "onRestart");
     }
 }
